deaa133003617cf6d8bf6e3af45eb53721a15fff
Kenny Millington <kenny@kennynet.co.uk>
2020-06-20 20:40:35 +0100

    Updates

---
+++ precache-manifest.f88d78f1ed6673d701370ca7e9c873f4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b80db5cb5ff4f8c4c3cc58740a5401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee9597527ca84ed4d96",
+    "url": "/static/css/2.d0e47e52.chunk.css"
+  },
+  {
+    "revision": "94da02ecdfad1f12ab64",
+    "url": "/static/css/main.2ba692e1.chunk.css"
+  },
+  {
+    "revision": "fee9597527ca84ed4d96",
+    "url": "/static/js/2.17769ddd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.17769ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94da02ecdfad1f12ab64",
+    "url": "/static/js/main.528e90e9.chunk.js"
+  },
+  {
+    "revision": "7a2399e78983f3c0c16d",
+    "url": "/static/js/runtime-main.ba114198.js"
+  },
+  {
+    "revision": "76927431573bd65f2639c44eb0087fe6",
+    "url": "/static/media/C.76927431.png"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "fdfc23b56cc0fe12ec2351df2f42f98b",
+    "url": "/static/media/background.fdfc23b5.jpg"
+  },
+  {
+    "revision": "31f926ac134297d62f4ef885ff83c526",
+    "url": "/static/media/bg-callout.31f926ac.jpg"
+  },
+  {
+    "revision": "c934a8a6d299bfa55c48589e25982a8c",
+    "url": "/static/media/bg-masthead.c934a8a6.jpg"
+  },
+  {
+    "revision": "1ee403edddb3c8ba09d9a30cb9f7d4a8",
+    "url": "/static/media/cache.1ee403ed.jpg"
+  },
+  {
+    "revision": "2103c44598b4a6454d3df6e57b3d1415",
+    "url": "/static/media/docker-logo.2103c445.png"
+  },
+  {
+    "revision": "3a15213ddffc497c558cabac9f1e2c2b",
+    "url": "/static/media/k8s-logo.3a15213d.svg"
+  },
+  {
+    "revision": "909568db8df68a327bf3d50f148f02b0",
+    "url": "/static/media/nodejs-logo.909568db.svg"
+  },
+  {
+    "revision": "932939fd6ce5f27e592f7a07c7422474",
+    "url": "/static/media/packaging.932939fd.jpg"
+  },
+  {
+    "revision": "2c8cc4a64e1339f56547b5831a0d0504",
+    "url": "/static/media/php-logo.2c8cc4a6.svg"
+  },
+  {
+    "revision": "5fb1ee685c4ff34f58c44e21a421dd15",
+    "url": "/static/media/puppet-logo.5fb1ee68.svg"
+  },
+  {
+    "revision": "85333030667c00acb9864298f7c6344a",
+    "url": "/static/media/python.85333030.png"
+  },
+  {
+    "revision": "4b84cd7c391fb2d2f132230ab8373d2c",
+    "url": "/static/media/recursive.4b84cd7c.jpg"
+  },
+  {
+    "revision": "f2d4a020205de5d66f2614c0310e23d1",
+    "url": "/static/media/remote-access.f2d4a020.jpg"
+  }
+]);
