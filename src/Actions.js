902a8ac9a9b48fa6686f6047202da793a87b7968
Kenny Millington <kenny@kennynet.co.uk>
2020-06-20 15:57:34 +0100

    Hide CV button

--- src/Actions.js
+++ src/Actions.js
@@ -6,7 +6,9 @@ export default function Actions()
         <section id="actions" className="content-section bg-primary text-white">
             <div className="container text-center">
                 <h2 className="mb-4">Find out more</h2>
+                { /* Currently not available
                 <a href="#" className="btn btn-xl btn-light mr-4">Download my CV</a>
+                */ }
                 <a href="https://www.linkedin.com/in/kennymillington" className="btn btn-xl btn-dark">Check out my LinkedIn</a>
             </div>
         </section>
