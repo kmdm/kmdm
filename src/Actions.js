3c4b65abea86ff8b3b3e76cc2bff8d7539130178
Kenny Millington <kenny@kennynet.co.uk>
2020-06-21 22:29:11 +0100

    Add CV

--- src/Actions.js
+++ src/Actions.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import cv from './assets/CV.pdf';
+
 export default function Actions()
 {
     return (
         <section id="actions" className="content-section bg-primary text-white">
             <div className="container text-center">
                 <h2 className="mb-4">Find out more</h2>
-                { /* Currently not available
-                <a href="#" className="btn btn-xl btn-light mr-4">Download my CV</a>
-                */ }
+                <a href={cv} className="btn btn-xl btn-light mr-4">Download my CV</a>
                 <a href="https://www.linkedin.com/in/kennymillington" className="btn btn-xl btn-dark">Check out my LinkedIn</a>
             </div>
         </section>
